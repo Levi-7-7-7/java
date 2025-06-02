325a105ad2d6f4398c5043d4d111b41350b43658
Lijo Thomas <lijothomas7777777@gmail.com>
2025-06-02 19:58:37 +0000

    Added files to  folder 3

---
+++ exp3/exp3.java
@@ -0,0 +1,29 @@
+class Box{
+
+    int l, b, h;
+    Box(int x, int y,int z){
+        l=x;
+        b=y;
+        h=z;
+    }
+
+    int calc_volume(){
+        int v = l*b*h;
+        return v;
+    }
+
+    int calc_area(){
+        int ar=2*(l*b+b*h+h*l);
+        return ar;
+    }
+}
+
+public class exp3{
+    public static void main(String[] args){
+        Box b1=new Box(5,5,5);
+        int V=b1.calc_volume();
+        int A=b1.calc_area();
+        System.out.println("Volume of the box is: "+V);
+        System.out.println("Area of the box is: "+A);
+    }
+}
