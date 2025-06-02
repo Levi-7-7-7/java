0dde520a09b54f89b335b5365ba40cd43122b69b
Lijo Thomas <lijothomas7777777@gmail.com>
2025-06-02 19:37:07 +0000

    initial commit

---
+++ exp1/exp1.java
@@ -0,0 +1,27 @@
+class rectangle{
+    int l, b, ar;
+
+    void get_data(int length, int breadth){
+        l=length;
+        b=breadth;
+    }
+
+    void area(){
+        ar=l*b;        
+    }
+
+    void display(){
+        System.out.println("length of the rectangle is: "+l);
+        System.out.println("breadth of the rectangle is: "+b);
+        System.out.println("area of the rectangle is: "+ar);
+    }
+}
+
+public class exp1{
+    public static void main(String[] args){
+        rectangle r=new rectangle();
+        r.get_data(5,2);
+        r.area();
+        r.display();
+    }
+}
---
+++ exp2/exp2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+class complex{
+    int r, i;
+    void get_data(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("real part: ");
+        r=sc.nextInt();
+        System.out.print("imaginary part: ");
+        i=sc.nextInt();
+    }
+    void display(){
+        System.out.println("complex number is: "+r+" + "+i+"i");        
+    }
+    complex add(complex c1,complex c2){
+        complex c3=new complex();
+        c3.r=c1.r+c2.r;
+        c3.i=c1.i+c2.i;
+        return c3;
+    }
+}
+
+public class exp2{
+    public static void main(String[] args){
+        complex c1 = new complex();
+        complex c2 = new complex();
+        complex A = new complex();
+        
+        System.out.print("\n");
+
+        System.out.println("enter the first complex number");
+        c1.get_data();
+        c1.display();
+
+        System.out.print("\n");
+
+        System.out.println("enter the second complex number");
+        c2.get_data();
+        c2.display();
+
+        System.out.print("\n");
+
+        A=A.add(c1,c2);
+        System.out.print("sum of the two ");
+        A.display();
+
+        System.out.println("\n");
+
+    }
+}
